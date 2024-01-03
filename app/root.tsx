b54e03bdab82e8197a74cd152504eb3ed7055006
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-01-03 18:53:53 +0100

    🌌 Added themeing!

--- app/root.tsx
+++ app/root.tsx
@@ -6,16 +6,34 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react'
+import type { LoaderFunctionArgs } from '@remix-run/node'
+import { json } from '@remix-run/node'
+
+import { ClientHints, getHints } from './components/ClientHint'
+import { useTheme } from './hooks/useTheme'
+import { getTheme } from './utils/server/theme.server'
 
 import './styles/tailwind.css'
 import './styles/fonts.css'
 
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  return json({
+    requestInfo: {
+      hints: getHints(request),
+      userPrefs: { theme: getTheme(request) },
+    },
+  })
+}
+
 export default function App() {
+  const theme = useTheme()
+
   return (
-    <html lang="en">
+    <html lang="en" className={`h-full overflow-x-hidden ${theme}`}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <ClientHints />
         <Meta />
         <Links />
       </head>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,8 @@
 import type { MetaFunction } from '@remix-run/node'
+import { useFetcher } from '@remix-run/react'
+
+import { useRequestInfo } from '~/hooks/useRequestInfo'
+import { useOptimisticThemeMode } from '~/hooks/useTheme'
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,9 +12,17 @@ export const meta: MetaFunction = () => {
 }
 
 export default function Index() {
+  const fetcher = useFetcher()
+  const requestInfo = useRequestInfo()
+  const optimisticMode = useOptimisticThemeMode()
+
+  const mode = optimisticMode ?? requestInfo.userPrefs.theme ?? 'system'
+
+  const availableThemes = ['light', 'dark', 'system']
+
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Remix (Updated)</h1>
       <ul>
         <li>
           <a
@@ -36,6 +48,28 @@ export default function Index() {
           </a>
         </li>
       </ul>
+      <div className="flex select-none flex-col rounded-2xl bg-white/60 px-1 py-1 drop-shadow-sm backdrop-blur-md transition hover:bg-white dark:bg-white/20">
+        <fetcher.Form method="POST" action={'/updateTheme'} className="flex">
+          <div className="flex items-center">
+            {availableThemes.map(theme => (
+              <button
+                key={theme}
+                type="submit"
+                name="theme"
+                value={theme}
+                className={`${
+                  mode === theme &&
+                  'rounded-xl bg-black/80 text-white drop-shadow-md dark:bg-white/80 dark:text-black'
+                } h-9 px-4`}
+              >
+                <span className="font-semibold">
+                  {theme && theme.charAt(0).toUpperCase() + theme.slice(1)}
+                </span>
+              </button>
+            ))}
+          </div>
+        </fetcher.Form>
+      </div>
     </div>
   )
 }
