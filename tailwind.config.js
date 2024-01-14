dda255ac404d36c8979ea3e44889634469827a1b
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-01-14 17:01:43 +0100

    💅 Unused palette (still a WIP)

--- tailwind.config.js
+++ tailwind.config.js
@@ -202,6 +202,20 @@ export default {
       }),
       colors: {
         'background-color': 'var(--background-color)',
+        // slate: {
+        //   50: 'var(--slate-50)',
+        //   100: 'var(--slate-100)',
+        //   200: 'var(--slate-200)',
+        //   300: 'var(--slate-300)',
+        //   400: 'var(--slate-400)',
+        //   500: 'var(--slate-500)',
+        //   600: 'var(--slate-600)',
+        //   700: 'var(--slate-700)',
+        //   800: 'var(--slate-800)',
+        //   900: 'var(--slate-900)',
+        //   950: 'var(--slate-950)',
+        //   1000: 'var(--slate-1000)',
+        // },
       },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
