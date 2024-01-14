93a131609336b33229244dfcdae8a1a5338f7733
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-01-14 17:03:00 +0100

    🌙 Remix PWA-styled theme switcher

---
+++ app/components/ThemeSwitcher.tsx
@@ -0,0 +1,35 @@
+import { useFetcher } from '@remix-run/react'
+import { MoonIcon, SunIcon } from 'lucide-react'
+
+import { useTheme } from '~/hooks/useTheme'
+
+export const RemixPWAThemeSwitcher = () => {
+  const fetcher = useFetcher()
+  const theme = useTheme()
+
+  return (
+    <button
+      className="flex h-6 w-6 items-center justify-center rounded-lg shadow-md shadow-black/5 ring-1 ring-black/5 dark:bg-slate-700 dark:ring-inset dark:ring-white/5"
+      onClick={() => {
+        if (theme === 'light') {
+          fetcher.submit(
+            { theme: 'dark' },
+            { method: 'post', action: '/updateTheme' }
+          )
+        } else if (theme === 'dark') {
+          fetcher.submit(
+            { theme: 'light' },
+            { method: 'post', action: '/updateTheme' }
+          )
+        }
+      }}
+    >
+      {theme === 'light' && (
+        <SunIcon className="h-4 w-4 fill-sky-400 text-sky-400" />
+      )}
+      {theme === 'dark' && (
+        <MoonIcon className="h-4 w-4 fill-sky-400 text-sky-400" />
+      )}
+    </button>
+  )
+}
