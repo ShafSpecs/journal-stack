d01a04773b91e2db697723c2ad26b797f030ad6c
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-01-22 16:15:46 +0100

    🔧 Fixed RDT bug

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [remix(), tsconfigPaths(), remixDevTools()],
+  server: {
+    open: true,
+    port: 3000,
+  },
 })
