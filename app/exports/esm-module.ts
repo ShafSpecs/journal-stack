afca5a78ae6a1d1aac4b228452a26ec863255b2b
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-01-24 22:23:24 +0100

    🧩 Remark & Rehype plugins export

--- app/exports/esm-module.ts
+++ app/exports/esm-module.ts
@@ -3,7 +3,6 @@ export const importRole = async () => import('../rehype/role')
 export const importCheckbox = async () => import('../rehype/checkbox')
 export const importHighlighter = async () => import('../remark/highlight')
 export const importGfm = async () => import('remark-gfm')
-// @ts-expect-error
 export const importEmoji = async () => import('remark-emoji')
 export const importSlug = async () => import('rehype-slug')
 
