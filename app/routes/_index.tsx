31c58339f2d5cef8e16300216e7aa05df1372db9
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-01-25 21:50:20 +0100

    ⛱️ New landing page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,10 @@
 import type { MetaFunction } from '@remix-run/node'
-import { redirect, useFetcher } from '@remix-run/react'
-
-import { useRequestInfo } from '~/hooks/useRequestInfo'
-import { useOptimisticThemeMode } from '~/hooks/useTheme'
+import { Link } from '@remix-run/react'
 
 export const loader = () => {
   // To add a landing page to your docs, remove this line.
-  return redirect('/docs/main')
+  // return redirect('/docs/main')
+  return null
 }
 
 export const meta: MetaFunction = () => {
@@ -17,64 +15,122 @@ export const meta: MetaFunction = () => {
 }
 
 export default function Index() {
-  const fetcher = useFetcher()
-  const requestInfo = useRequestInfo()
-  const optimisticMode = useOptimisticThemeMode()
-
-  const mode = optimisticMode ?? requestInfo.userPrefs.theme ?? 'system'
-
-  const availableThemes = ['light', 'dark', 'system']
-
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix (Updated)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-      <div className="flex select-none flex-col rounded-2xl bg-white/60 px-1 py-1 drop-shadow-sm backdrop-blur-md transition hover:bg-white dark:bg-white/20">
-        <fetcher.Form method="POST" action={'/updateTheme'} className="flex">
-          <div className="flex items-center">
-            {availableThemes.map(theme => (
-              <button
-                key={theme}
-                type="submit"
-                name="theme"
-                value={theme}
-                className={`${
-                  mode === theme &&
-                  'rounded-xl bg-black/80 text-white drop-shadow-md dark:bg-white/80 dark:text-black'
-                } h-9 px-4`}
-              >
-                <span className="font-semibold">
-                  {theme && theme.charAt(0).toUpperCase() + theme.slice(1)}
+    <div className="placeholder-index relative h-screen w-screen dark:bg-white sm:pb-16 sm:pt-8">
+      <div className="relative mx-auto max-w-[90rem] sm:px-6 lg:px-8">
+        <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+          <section className="absolute inset-0">
+            <img
+              className="h-full w-full object-cover"
+              src="/AI-Hero.png"
+              alt="Rushing vibes of life"
+            />
+            <div className="absolute inset-0 bg-slate-950/75 mix-blend-multiply" />
+          </section>
+          <section className="lg:pb-18 relative px-4 pb-8 pt-16 sm:px-6 sm:pb-14 sm:pt-24 lg:px-8 lg:pt-32">
+            <h1 className="overflow-hidden text-center text-7xl font-medium sm:text-6xl lg:text-8xl">
+              <span className="font-space block pb-2 pr-2 text-center uppercase text-white drop-shadow-md">
+                <span className="text-center">Journal&nbsp;</span>
+                <span className="block h-full bg-gradient-to-tr from-indigo-500 to-sky-300 bg-clip-text pb-1 pr-1 text-center text-transparent dark:from-indigo-500 dark:to-sky-300 sm:inline sm:pb-0">
+                  Stack
                 </span>
-              </button>
-            ))}
-          </div>
-        </fetcher.Form>
+              </span>
+            </h1>
+            <p className="mx-auto mt-8 max-w-lg text-center text-lg text-white sm:max-w-3xl md:mt-12">
+              Check the{' '}
+              <a
+                href="https://github.com/ShafSpecs/rockspec-stack/blob/main/README.md"
+                target={'_blank'}
+                className="text-white no-underline focus:text-white"
+                rel="noreferrer"
+              >
+                README.md
+              </a>{' '}
+              file for instructions on how to get this project deployed.
+            </p>
+            <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+              <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+                <Link
+                  to="/docs/main"
+                  className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-slate-800 shadow-sm hover:bg-blue-50 sm:px-8"
+                >
+                  Get Started
+                </Link>
+                <a
+                  aria-label="GitHub"
+                  href="https://github.com/ShafSpecs/journal-stack"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flex items-center justify-center rounded-md bg-slate-700 px-4 py-3 font-medium text-white hover:bg-slate-800  "
+                >
+                  Source Code
+                </a>
+              </div>
+            </div>
+            <a href="https://remix.run" className="mx-auto w-max">
+              <img
+                src="/icons/remix.svg"
+                alt="Remix"
+                className="mx-auto mt-16 w-full max-w-[12rem] md:max-w-[16rem]"
+              />
+            </a>
+          </section>
+        </div>
       </div>
+      <div className="mx-auto max-w-7xl px-8 py-2 sm:px-12 lg:px-16">
+        <div className="mt-6 flex flex-wrap justify-center gap-8">
+          {[
+            {
+              src: 'https://user-images.githubusercontent.com/1500684/157764397-ccd8ea10-b8aa-4772-a99b-35de937319e1.svg',
+              alt: 'Fly.io',
+              href: 'https://fly.io',
+            },
+            {
+              src: 'https://mdx-logo.now.sh',
+              alt: 'MDX',
+              href: 'https://mdxjs.com',
+            },
+            {
+              src: 'https://user-images.githubusercontent.com/1500684/157764276-a516a239-e377-4a20-b44a-0ac7b65c8c14.svg',
+              alt: 'Tailwind',
+              href: 'https://tailwindcss.com',
+            },
+            // {
+            //   src: "https://user-images.githubusercontent.com/1500684/157772447-00fccdce-9d12-46a3-8bb4-fac612cdc949.svg",
+            //   alt: "Vitest",
+            //   href: "https://vitest.dev",
+            // },
+            {
+              src: 'https://user-images.githubusercontent.com/1500684/157772934-ce0a943d-e9d0-40f8-97f3-f464c0811643.svg',
+              alt: 'Prettier',
+              href: 'https://prettier.io',
+            },
+            {
+              src: 'https://user-images.githubusercontent.com/1500684/157772990-3968ff7c-b551-4c55-a25c-046a32709a8e.svg',
+              alt: 'ESLint',
+              href: 'https://eslint.org',
+            },
+            {
+              src: 'https://user-images.githubusercontent.com/1500684/157773063-20a0ed64-b9f8-4e0b-9d1e-0b65a3d4a6db.svg',
+              alt: 'TypeScript',
+              href: 'https://typescriptlang.org',
+            },
+          ].map(img => (
+            <a
+              key={img.href}
+              href={img.href}
+              target="_blank"
+              className="flex h-16 w-32 justify-center p-1 grayscale transition hover:grayscale-0 focus:grayscale-0"
+              rel="noreferrer"
+            >
+              <img alt={img.alt} src={img.src} />
+            </a>
+          ))}
+        </div>
+      </div>
+      <section className="absolute bottom-2 mb-2 mt-2 w-full pb-3 text-center md:mb-0 md:mt-0">
+        Crafted by ShafSpecs with ❤️ and Remix vibes 💿
+      </section>
     </div>
   )
 }
