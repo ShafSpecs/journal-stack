ac8436fc3552cce8ab8e4276911d0dcfd4c0ce99
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-01-27 00:29:32 +0100

    📱 Added Remix PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,17 @@ import { unstable_vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
 import { remixDevTools } from 'remix-development-tools/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { unstable_RemixPWA as remixPWA } from '@remix-pwa/dev'
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths(), remixDevTools()],
+  plugins: [
+    remix(),
+    tsconfigPaths(),
+    remixDevTools(),
+    remixPWA({
+      workerMinify: true,
+    }),
+  ],
   server: {
     open: true,
     port: 3000,
