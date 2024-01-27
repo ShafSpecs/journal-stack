9700777fe0e5951421c95a20e0f504f728f5fce6
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-01-27 13:15:30 +0100

    📵 Removed Remix PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,9 @@ import { unstable_vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
 import { remixDevTools } from 'remix-development-tools/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { unstable_RemixPWA as remixPWA } from '@remix-pwa/dev'
 
 export default defineConfig({
-  plugins: [
-    remix(),
-    tsconfigPaths(),
-    remixDevTools(),
-    remixPWA({
-      workerMinify: true,
-    }),
-  ],
+  plugins: [remix(), tsconfigPaths(), remixDevTools()],
   server: {
     open: true,
     port: 3000,
