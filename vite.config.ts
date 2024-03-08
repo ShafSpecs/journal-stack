b83b6ca81572aced44359691c2878e903d61f749
Alem Tuzlak <t.zlak@hotmail.com>
2024-03-08 15:24:48 +0100

    Stabilized Remix Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { unstable_RemixPWA } from '@remix-pwa/dev'
-import { unstable_vitePlugin as remix } from '@remix-run/dev'
+import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
 import { remixDevTools } from 'remix-development-tools/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths(), remixDevTools(), unstable_RemixPWA()],
+  plugins: [remixDevTools(), remix(), tsconfigPaths(), unstable_RemixPWA()],
   server: {
     open: true,
     port: 3000,
